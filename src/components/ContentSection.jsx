4406e53332e3f0111f00d3bf465fc908436c020a
Edd-wordd <edward.plasencio@gmail.com>
2021-09-23 21:47:31 -0700

    add features/major signs section

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -15,9 +15,17 @@ const useStyles = makeStyles((theme) => ({
     backgroundPosition: 'center center',
     padding: '20rem 0rem 5rem 0rem',
   },
+  mainContent: {
+    padding: '4rem 0rem 8rem 0rem',
+  },
+  mainTitle: {
+    borderBottom: '3px solid #FF8E53',
+  },
   pics: {
     width: '600px',
     height: 'auto',
+    padding: '1.5rem',
+    border: '3px solid pink',
   },
   mainDescription: {
     width: '700px',
@@ -33,6 +41,10 @@ const useStyles = makeStyles((theme) => ({
     height: 'auto',
     color: mainTheme.palette.defaultLight.main,
   },
+  banner: {
+    background: 'linear-gradient(40deg,#9b80ce, #f4f1fa 50%,#0f0c29 30%)',
+    padding: '4rem 0rem 4rem 0rem',
+  },
 }))
 
 function ContentSection() {
@@ -45,6 +57,7 @@ function ContentSection() {
           direction="row"
           justify="space-around"
           alignItems="center"
+          className={classes.mainContent}
         >
           <Box>
             <img
@@ -53,7 +66,7 @@ function ContentSection() {
             />
           </Box>
           <Grid>
-            <Typography variant="h3">
+            <Typography variant="h3" className={classes.mainTitle}>
               <DescriptionIcon className={classes.icons} />
               Content Writing Essentials
             </Typography>
@@ -65,6 +78,46 @@ function ContentSection() {
               adipisicing elit. Incidunt sit quae et dolorum consequuntur
               similique quo nam laudantium. Distinctio possimus facilis
               inventore numquam ad id soluta amet quisquam non asperiores.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="center"
+          className={classes.banner}
+        >
+          <Grid>
+            <Typography variant="h4">Features</Typography>
+            <Typography>
+              <DescriptionIcon /> This is the first feature
+            </Typography>{' '}
+            <Typography>
+              <DescriptionIcon /> This is the first feature
+            </Typography>{' '}
+            <Typography>
+              <DescriptionIcon /> This is the first feature
+            </Typography>{' '}
+            <Typography>
+              <DescriptionIcon /> This is the first feature
+            </Typography>
+          </Grid>
+          <Grid>
+            <Typography variant="h4">
+              Major signs you need Content Writing
+            </Typography>
+            <Typography>
+              <DescriptionIcon /> This is the first feature
+            </Typography>{' '}
+            <Typography>
+              <DescriptionIcon /> This is the first feature
+            </Typography>{' '}
+            <Typography>
+              <DescriptionIcon /> This is the first feature
+            </Typography>{' '}
+            <Typography>
+              <DescriptionIcon /> This is the first feature
             </Typography>
           </Grid>
         </Grid>
