86ae0fb159ed5cd372a1b6d1d7372173cb4cabec
Edd-wordd <edward.plasencio@gmail.com>
2021-09-23 22:12:49 -0700

    footer added to contentwriting section

--- src/pages/ContentWriting.jsx
+++ src/pages/ContentWriting.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ContentSection from '../components/ContentSection'
+import Footer from '../components/Footer'
 
 function ContentWriting() {
   return (
     <div>
       <ContentSection />
+      <Footer />
     </div>
   )
 }
