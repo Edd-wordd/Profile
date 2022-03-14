80f4a694c5b268f8ffb74f65a893fa4b3d277c85
Edd-wordd <edward.plasencio@gmail.com>
2022-03-13 19:38:50 -0700

    update major sign component to receive correct data through props

--- src/components/sections/MajorSigns.jsx
+++ src/components/sections/MajorSigns.jsx
@@ -1,131 +1,107 @@
-import React from 'react';
+import React from 'react'
 import { SectionHeader } from '../index'
-import { Grid, Typography, Container, Paper, Hidden } from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import mainTheme from "../../theme";
-
+import { Grid, Typography, Container, Paper, Hidden } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import mainTheme from '../../theme'
 
 const useStyles = makeStyles((theme) => ({
-    root:{
+  root: {
+    padding: theme.spacing(2),
+    color: mainTheme.palette.defaultLight.main,
+  },
+  information: {
+    color: mainTheme.palette.defaultLight.main,
+    padding: '2rem 10rem',
+    [theme.breakpoints.down('md')]: {
       padding: theme.spacing(2),
     },
-    header:{
-        color: mainTheme.palette.defaultLight.main,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: theme.spacing(4),
+    color: theme.palette.text.secondary,
+    borderRadius: theme.spacing(1),
+    boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)',
+    [theme.breakpoints.down('md')]: {
+      margin: theme.spacing(1),
     },
-    information: {
-        color: mainTheme.palette.defaultLight.main,
-        padding: '2rem 10rem',
-        [theme.breakpoints.down('md')]: {
-            padding: theme.spacing(2),
-        },
+  },
+  image: {
+    margin: theme.spacing(0.5),
+    borderRadius: theme.spacing(1),
+    width: '400px',
+    height: '400px',
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
     },
-    paper:{
-        padding: theme.spacing(2),
-        margin: theme.spacing(4),
-        color: theme.palette.text.secondary,
-        borderRadius: '10px',
-        boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)',
-        [theme.breakpoints.down('md')]: {
-            margin: theme.spacing(2),
-        },
+  },
+  paperTitle: {
+    fontSize: theme.spacing(3),
+    fontWeight: 'bold',
+    color: theme.palette.text.primary,
+    marginBottom: theme.spacing(1),
+    [theme.breakpoints.down('md')]: {
+      fontSize: theme.spacing(2),
     },
-    image:{
-        margin: theme.spacing(.5),
-        borderRadius: '10px',
-        width: '400px',
-        height: 'auto',
-        [theme.breakpoints.down('md')]: {
-            width: '100%',
-        },
-    },
-    paperTitle:{
-        fontSize: '1.5rem',
-        fontWeight: 'bold',
-        color: theme.palette.text.primary,
-        marginBottom: theme.spacing(1),
-        [theme.breakpoints.down('md')]: {
-            fontSize: '1.2rem',
-        },
-    },
-
-
+  },
 }))
-const MajorSigns = ()=>{
-    const classes = useStyles();
-    return(
-        <div className={classes.root}>
-        <SectionHeader className={classes.header} title="Major Signs you need 'input service'" subTitle='here is what you need'/>
-            <Container maxWidth='lg'>
+const MajorSigns = (props) => {
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <SectionHeader
+        title={`Major Signs you need ${props.title}`}
+        subTitle="here is what you need"
+      />
+      <Container maxWidth="lg">
         <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            className={classes.information}
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          className={classes.information}
         >
-            <Typography>
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid atque aut, ea error
-                    fuga illo incidunt inventore ipsum iure iusto molestiae nihil pariatur sint suscipit totam unde
-                    veniam, veritatis.
-
-            </Typography>
+          <Typography>{props.majorSignInfo}</Typography>
         </Grid>
 
-                <Grid container direction='row' justify='center' alignItems='center'>
-                    <Grid lg={4}>
-                    <Paper className={classes.paper}>
-                        <Typography variant='h5' className={classes.paperTitle}>
-                         Title Name
-                        </Typography>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid atque aut, ea error
-                            fuga illo incidunt inventore ipsum iure iusto molestiae nihil pariatur sint suscipit totam unde
-                            veniam, veritatis.
-                        </Typography>
-                    </Paper>
-                        <Paper className={classes.paper}>
-                            <Typography variant='h5' className={classes.paperTitle}>
-                                Title Name
-                            </Typography>
-                            <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid atque aut, ea error
-                                fuga illo incidunt inventore ipsum iure iusto molestiae nihil pariatur sint suscipit totam unde
-                                veniam, veritatis.
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                    <Hidden mdDown>
-                    <Grid lg={4}>
-                    <img  className={classes.image} alt='image3' src="../../../public/assets/computerVector.jpg"/>
-                    </Grid>
-                    </Hidden>
-                    <Grid lg={4}>
-                        <Paper className={classes.paper}>
-                            <Typography variant='h5' className={classes.paperTitle}>
-                                Title Name
-                            </Typography>
-                            <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid atque aut, ea error
-                                fuga illo incidunt inventore ipsum iure iusto molestiae nihil pariatur sint suscipit totam unde
-                                veniam, veritatis.
-                            </Typography>
-                        </Paper>
-                        <Paper className={classes.paper}>
-                            <Typography variant='h5' className={classes.paperTitle}>
-                                Title Name
-                            </Typography>
-                            <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid atque aut, ea error
-                                fuga illo incidunt inventore ipsum iure iusto molestiae nihil pariatur sint suscipit totam unde
-                                veniam, veritatis.
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                </Grid>
-
-            </Container>
-        </div>
-    )
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Grid lg={4} item>
+            <Paper className={classes.paper}>
+              <Typography variant="h5" className={classes.paperTitle}>
+                {props.majorTitle_1}
+              </Typography>
+              <Typography>{props.majorInfo_1}</Typography>
+            </Paper>
+            <Paper className={classes.paper}>
+              <Typography variant="h5" className={classes.paperTitle}>
+                {props.majorTitle_2}
+              </Typography>
+              <Typography>{props.majorInfo_2}</Typography>
+            </Paper>
+          </Grid>
+          <Hidden mdDown>
+            <Grid lg={4} item>
+              <img className={classes.image} alt="image3" src={props.majorImage} />
+            </Grid>
+          </Hidden>
+          <Grid lg={4} item>
+            <Paper className={classes.paper}>
+              <Typography variant="h5" className={classes.paperTitle}>
+                {props.majorTitle_3}
+              </Typography>
+              <Typography>{props.majorInfo_3}</Typography>
+            </Paper>
+            <Paper className={classes.paper}>
+              <Typography variant="h5" className={classes.paperTitle}>
+                {props.majorTitle_4}
+              </Typography>
+              <Typography>{props.majorInfo_4}</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  )
 }
 
-export default MajorSigns;
+export default MajorSigns
