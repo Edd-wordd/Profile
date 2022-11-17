5c08a84ca1f0b31f0018398986e8381c0143fbc6
Edd-wordd <edward.plasencio@gmail.com>
2022-11-16 17:38:44 -0800

    separate styles from every component on about us page

--- src/components/sections/WhatWeDo.jsx
+++ src/components/sections/WhatWeDo.jsx
@@ -1,65 +1,10 @@
 import React from 'react'
-import mainTheme from '../../theme'
-import { makeStyles } from '@material-ui/core/styles'
-import { Container, Typography, Grid, Link } from '@material-ui/core'
-import SectionHeader from './SectionHeader'
-import Paper from '@material-ui/core/Paper'
+import { Container, Typography, Grid, Link, Paper, Slide } from '@material-ui/core'
+import { SectionHeader } from '../index'
 import TypeIt from 'typeit-react'
 import CheckCircleIcon from '@material-ui/icons/CheckCircle'
 import { Transition } from 'react-transition-group'
-import Slide from '@material-ui/core/Slide'
-
-const useStyles = makeStyles((theme) => ({
-  root: {},
-  paragraphWrapper: { margin: '3rem 0' },
-  paraInfo: {
-    letterSpacing: theme.spacing(0.25),
-    lineHeight: theme.spacing(0.25),
-  },
-  boxes: {
-    width: '600px',
-    height: 'auto',
-    padding: theme.spacing(2),
-    margin: theme.spacing(1),
-    marginBottom: theme.spacing(3),
-    [theme.breakpoints.down('xs')]: {
-      width: '100%',
-    },
-  },
-  box1: {
-    width: '600px',
-    height: 'auto',
-    border: 'none',
-    padding: '5rem 1.5rem',
-    marginBottom: theme.spacing(1),
-    [theme.breakpoints.down('xs')]: {
-      width: '100%',
-    },
-  },
-  box2: {
-    width: '600px',
-    height: 'auto',
-    border: 'none',
-    padding: '3rem 1.5rem',
-
-    marginBottom: theme.spacing(1),
-    [theme.breakpoints.down('xs')]: {
-      width: '100%',
-    },
-  },
-
-  icons: {
-    color: ' #FE6B8B',
-    width: '50px',
-    height: 'auto',
-    padding: '1rem 0rem 0rem 2rem',
-  },
-  bulletPoint: {
-    fontWeight: 700,
-    fontSize: '1.2rem',
-  },
-  coreValue: { padding: '1rem 0rem 0rem 0rem' },
-}))
+import { useStyles } from '../styles/WhatWeDo.styles'
 
 const bulletPoints = [
   {
@@ -106,8 +51,8 @@ const servicePoints = [
   },
 ]
 
-function WhatWeDo() {
-  const classes = useStyles()
+function WhatWeDo(props) {
+  const classes = useStyles(props)
   const [inProp, setInProp] = React.useState(false)
 
   const handleChange = () => {
@@ -128,30 +73,21 @@ function WhatWeDo() {
             <b>THese little details make the diference</b>
           </Typography>
           <Typography paragraph variant="body1">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad
-            rerum possimus modi repudiandaeorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Dolor ad rerum possimus modi
-            repudiandaeorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Dolor ad rerum possimus modi repudiandaeorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Dolor ad rerum possimus modi
-            repudiandaeorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Dolor ad rerum possimus modi repudiandaeorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Dolor ad rerum possimus modi
-            repudiandaeorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Dolor ad rerum possimus modi repudiandaeorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Dolor ad rerum possimus modi
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad rerum possimus modi
+            repudiandaeorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor ad rerum
+            possimus modi repudiandaeorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            ad rerum possimus modi repudiandaeorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Dolor ad rerum possimus modi repudiandaeorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Dolor ad rerum possimus modi repudiandaeorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Dolor ad rerum possimus modi repudiandaeorem, ipsum dolor
+            sit amet consectetur adipisicing elit. Dolor ad rerum possimus modi repudiandaeorem,
+            ipsum dolor sit amet consectetur adipisicing elit. Dolor ad rerum possimus modi
             repudiandae
           </Typography>
         </Paper>
         <Paper variant="outlined" className={classes.box2}>
           {bulletPoints.map((value, index) => (
-            <Grid
-              container
-              direction="row"
-              justify="flex-start"
-              alignItems="center"
-              key={index}
-            >
+            <Grid container direction="row" justify="flex-start" alignItems="center" key={index}>
               <CheckCircleIcon className={classes.icons} />
               <Typography variant="body1" className={classes.coreValue}>
                 <TypeIt
@@ -171,17 +107,15 @@ function WhatWeDo() {
         <Container maxWidth="lg">
           <Grid className={classes.paragraphWrapper}>
             <Typography paragraph variant="body1" className={classes.paraInfo}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
-              officiis ea! Atque, ratione cumque. Fugiat earum eius doloribus
-              quo repudiandae, maiores excepturi officia iusto consequuntur quis
-              odio labore, commodi minima? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Deserunt, officiis ea! Atque,
-              ratione cumque. Fugiat earum eius doloribus quo repudiandae,
-              maiores excepturi officia iusto consequuntur quis odio labore,
-              commodi minima? Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Deserunt, officiis ea! Atque, ratione cumque. Fugiat earum
-              eius doloribus quo repudiandae, maiores excepturi officia iusto
-              consequuntur quis odio labore, commodi minima?8
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, officiis ea! Atque,
+              ratione cumque. Fugiat earum eius doloribus quo repudiandae, maiores excepturi officia
+              iusto consequuntur quis odio labore, commodi minima? Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Deserunt, officiis ea! Atque, ratione cumque. Fugiat
+              earum eius doloribus quo repudiandae, maiores excepturi officia iusto consequuntur
+              quis odio labore, commodi minima? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Deserunt, officiis ea! Atque, ratione cumque. Fugiat earum eius doloribus quo
+              repudiandae, maiores excepturi officia iusto consequuntur quis odio labore, commodi
+              minima?8
             </Typography>
           </Grid>
         </Container>
@@ -194,11 +128,7 @@ function WhatWeDo() {
                   <Typography variant="subtitle1">
                     <b>{service.title}</b>
                   </Typography>
-                  <Typography
-                    paragraph
-                    variant="body1"
-                    className={classes.coreValue}
-                  >
+                  <Typography paragraph variant="body1" className={classes.coreValue}>
                     {service.details}
                     <Link> Learn More</Link>
                   </Typography>
---
+++ src/components/styles/GetInTouch.styles.js
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core'
+import mainTheme from '../../theme'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: 'linear-gradient(45deg,#CC95C0,#1FA2FF  , #12D8FA , #A6FFCB )',
+    color: mainTheme.palette.defaultLight,
+    padding: theme.spacing(1),
+  },
+  h1: {
+    textAlign: 'left',
+    lineHeight: theme.spacing(0.15),
+    letterSpacing: theme.spacing(0.25),
+    color: mainTheme.palette.text.primary,
+  },
+  paragraph: {
+    paddingTop: theme.spacing(3),
+    color: mainTheme.palette.text.primary,
+    fontStyle: 'italic',
+    alignSelf: 'flex-start',
+  },
+  logo: {
+    maxWidth: '75%',
+    height: 'auto',
+  },
+}))
+
+export { useStyles }
