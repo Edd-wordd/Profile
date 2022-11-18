fa9489ceb3cb7a7cab4c0281cc69846e4c92dfb2
Edd-wordd <edward.plasencio@gmail.com>
2022-11-17 16:51:19 -0800

    refactor styles from all pages

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles(() => ({
-  root: {
-    //  margin: '7rem 0 0 0',
-  },
-}))
 
 function Portfolio() {
-  const classes = useStyles()
-
   return (
-    <div className={classes.root}>
+    <div>
       <h1>this is the portfolio page</h1>
-
     </div>
   )
 }
