eb1a020752bf86da353e9b7182ff9582ab8440b5
Edd-wordd <edward.plasencio@gmail.com>
2022-11-27 17:39:22 -0800

    install deps

--- src/components/buttons/ButtonLink.jsx
+++ src/components/buttons/ButtonLink.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { Backdrop, Fade, Button, Grid, Modal, Link, Typography, TextField } from '@material-ui/core'
+import { Fade, Button, Grid, Modal, Link, Typography, TextField } from '@material-ui/core'
 import ComputerIcon from '@material-ui/icons/Computer'
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt'
 import { useStyles } from '../styles/buttons/ButtonLink.styles'
-import { ColorRing } from 'react-loader-spinner'
+// import { ColorRing } from 'react-loader-spinner'
+import { Backdrop } from '@mui/material'
 
 const textInputs = [
   {
@@ -55,17 +56,17 @@ const textInputs = [
 function ButtonLink(props) {
   const classes = useStyles(props)
   const [open, setOpen] = React.useState(false)
-  const [loading, setLoading] = useState(false)
-  const [show, setShow] = useState(false)
+  // const [loading, setLoading] = useState(false)
+  // const [show, setShow] = useState(false)
 
-  const LoadSpinner = () => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-      setShow(!show)
-    }, 700)
-    handleOpen()
-  }
+  // const LoadSpinner = () => {
+  //   setLoading(true)
+  //   setTimeout(() => {
+  //     setLoading(false)
+  //     setShow(!show)
+  //   }, 700)
+  //   handleOpen()
+  // }
 
   const handleOpen = () => {
     setOpen(true)
@@ -91,60 +92,62 @@ function ButtonLink(props) {
           className={classes.btn}
           endIcon={<ArrowRightAltIcon className={classes.BtnIcon} />}
           size="large"
-          onClick={LoadSpinner}
+          // onClick={LoadSpinner}
+          onClick={handleOpen}
         >
           {props.btnTwoText}
         </Button>
-        {loading ? (
-          <ColorRing isible={true} ariaLabel="blocks-loading" wrapperClass={classes.spinner} />
-        ) : (
-          <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            className={classes.modal}
-            open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500,
-            }}
-          >
-            <Fade in={open}>
-              <div className={classes.paper}>
-                <Typography className={classes.formHeader}>
-                  Talk to an expert about your project!
-                </Typography>
-                <form className={classes.form} noValidate autoComplete="off">
-                  {textInputs.map((value, index) => (
-                    <TextField
-                      className={classes.textField}
-                      key={index}
-                      required={value.required}
-                      id={value.id}
-                      label={value.label}
-                      variant={value.variant}
-                      size={value.size}
-                      multiline={value.multiline}
-                      rowsMax={value.rowsMax}
-                      style={value.style}
-                      fullWidth={value.fullWidth}
-                    />
-                  ))}
-                  <Button
-                    className={classes.submitBtn}
-                    size="large"
-                    variant="outlined"
-                    aria-label="large outlined button"
-                    onClick={() => submitForm()}
-                  >
-                    SUBMIT
-                  </Button>
-                </form>
-              </div>
-            </Fade>
-          </Modal>
-        )}
+        {/*{loading ? (*/}
+        {/*  <ColorRing isible={true} ariaLabel="blocks-loading" wrapperClass={classes.spinner} />*/}
+        {/*) : (*/}
+
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <Typography className={classes.formHeader}>
+                Talk to an expert about your project!
+              </Typography>
+              <form className={classes.form} noValidate autoComplete="off">
+                {textInputs.map((value, index) => (
+                  <TextField
+                    className={classes.textField}
+                    key={index}
+                    required={value.required}
+                    id={value.id}
+                    label={value.label}
+                    variant={value.variant}
+                    size={value.size}
+                    multiline={value.multiline}
+                    rowsMax={value.rowsMax}
+                    style={value.style}
+                    fullWidth={value.fullWidth}
+                  />
+                ))}
+                <Button
+                  className={classes.submitBtn}
+                  size="large"
+                  variant="outlined"
+                  aria-label="large outlined button"
+                  onClick={() => submitForm()}
+                >
+                  SUBMIT
+                </Button>
+              </form>
+            </div>
+          </Fade>
+        </Modal>
+        {/*)}*/}
       </Grid>
     </div>
   )
