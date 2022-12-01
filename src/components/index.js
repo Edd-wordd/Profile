936dbf57328510cb284172be02fb88276cac3fc7
Edd-wordd <edward.plasencio@gmail.com>
2022-11-30 17:35:56 -0800

    twitter icon added to footer

--- src/components/index.js
+++ src/components/index.js
@@ -37,6 +37,7 @@ export { default as TechUsed } from '../components/sections/TechUsed'
 export { default as WhatWeDo } from '../components/sections/WhatWeDo'
 export { default as WhoWeAre } from '../components/sections/WhoWeAre'
 export { default as MajorSigns } from '../components/sections/MajorSigns'
+
 //============================================
 // Forms
 //============================================
