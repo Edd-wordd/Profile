7f1d6676401d26d56ec5a63cdcc911a36c85c5c7
Edd-wordd <edward.plasencio@gmail.com>
2022-12-17 22:41:23 -0800

    maindisplay btn styling

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -7,8 +7,8 @@ function AboutUs() {
       <MainDisplay
         mobileViewTitle="The story of us!"
         mobileViewSubtitle="lets go get this"
-        mainViewTitle="The story of us!"
-        mainViewSubtitle="lets go get this bitches"
+        mainViewTitle="A guaranteed method to building your business!"
+        mainViewSubtitle="-Letting you focus on what you do best"
         href={`https://images.unsplash.com/photo-1581090700227-1e37b190418e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80`}
         btnOneText="Hello"
       />
