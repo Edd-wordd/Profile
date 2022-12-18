e4d7a43b567f31424144d50862e1a59bd1c2fe1a
Edd-wordd <edward.plasencio@gmail.com>
2022-12-17 18:38:46 -0800

    refactor contact us form

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,7 +9,8 @@ function Home() {
         mobileViewSubtitle="lets go get this"
         mainViewTitle="Look to the future..."
         mainViewSubtitle="lets go baby!!!"
-        href={`https://images.unsplash.com/photo-1508361727343-ca787442dcd7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=$%7BimageWidth%7D&q=80`}
+        // href={`https://images.unsplash.com/photo-1508361727343-ca787442dcd7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=$%7BimageWidth%7D&q=80`}
+        href={`https://images.unsplash.com/photo-1535378620166-273708d44e4c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=957&q=80`}
         buttonOneText={'Web Development'}
         buttonLink={'/services/web-design'}
         buttonTwoText={'SEO Services'}
