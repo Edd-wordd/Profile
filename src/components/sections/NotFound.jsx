dc98cc5e1c2a41717be1cc124820a6ba053c565a
Edd-wordd <edward.plasencio@gmail.com>
2022-12-28 21:17:53 -0800

    refactor 404 page

--- src/components/sections/NotFound.jsx
+++ src/components/sections/NotFound.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {useStyles} from "../styles/sections/NotFound.styles"
 
 const NotFound = () => {
+  const classes = useStyles()
   return (
-    <div>
+    <div className={classes.errorWrapper}>
       <h1>404</h1>
       <p>Page not found</p>
     </div>
