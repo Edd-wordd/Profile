d2a98c98dcfa2d295ae932f4187336994d3dca91
Edd-wordd <edward.plasencio@gmail.com>
2023-01-09 13:19:32 -0800

    styling adjustment who we are

--- src/components/styles/sections/WhoWeAre.styles.js
+++ src/components/styles/sections/WhoWeAre.styles.js
@@ -1,14 +1,13 @@
 import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
   paragraphWrapper: { margin: '3rem 0' },
   paraInfo: {
     letterSpacing: theme.spacing(0.25),
     lineHeight: theme.spacing(0.25),
   },
   boxes: {
-    width: '250px',
+    width: theme.spacing(35),
     height: 'auto',
     padding: theme.spacing(4),
     marginBottom: theme.spacing(4),
