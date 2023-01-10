483c029a64d7fa833ffd34faa736733f002f024b
Edd-wordd <edward.plasencio@gmail.com>
2023-01-10 12:46:52 -0800

    about us code refactor DRY

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MainDisplay, MissionStatement, TechUsed, GetInTouch, WhatWeDo, WhoWeAre } from '../index'
+import { MainDisplay, MissionStatement, GetInTouch, WhatWeDo, Resources } from "../index";
 
 function AboutUs() {
   return (
@@ -11,11 +11,9 @@ function AboutUs() {
         mainViewSubtitle="-Letting you focus on what you do best"
         href={`https://images.unsplash.com/photo-1600049780189-60f39be1949f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80`}
       />
-
-      <WhoWeAre />
-      <MissionStatement />
       <WhatWeDo />
-      <TechUsed />
+      <MissionStatement />
+      <Resources />
       <GetInTouch />
     </>
   )
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -18,7 +18,6 @@ function Home() {
       <WhoWeAre />
       <HmServices />
       <Questions />
-      <Resources />
       <GetStarted />
     </div>
   )
