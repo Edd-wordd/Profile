97d969727add88de1e70e93d4b4406892ae26d8b
Edd-wordd <edward.plasencio@gmail.com>
2023-01-09 19:05:06 -0800

    remove unsed code

--- src/components/sections/Questions.jsx
+++ src/components/sections/Questions.jsx
@@ -1,36 +1,32 @@
 import React from 'react'
-import { Container, Typography, Grid, Link, Paper, Fade } from '@material-ui/core'
-import PersonIcon from '@material-ui/icons/Person'
-import BackupIcon from '@material-ui/icons/Backup'
-import { Transition } from 'react-transition-group'
+import { Container, Typography, Grid, Link, Paper} from '@material-ui/core'
 import { SectionHeader } from '../index'
-import CheckBoxIcon from '@material-ui/icons/CheckBox'
-import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined'
-import BlurOnOutlinedIcon from '@material-ui/icons/BlurOnOutlined'
-import PostAddOutlinedIcon from '@material-ui/icons/PostAddOutlined'
-import CameraEnhanceOutlinedIcon from '@material-ui/icons/CameraEnhanceOutlined'
 import { useStyles } from '../styles/sections/Questions.styles'
 
 const sectionOneQuestions = [
   {
+    id: 2000,
     question: 'Do you have a great mobile app idea?',
     details:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     link: '/services/mobile-app',
   },
   {
+    id: 2001,
     question: 'Does your business need a online presence?',
     details:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     link: '/services/web-design',
   },
   {
+    id: 2002,
     question: 'Is your site outdated and in need of a revamp?',
     details:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
     link: '/services/web-maintenance',
   },
   {
+    id: 2003,
     question: 'Are you not getting the traction you expected?',
     details:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
@@ -38,75 +34,18 @@ const sectionOneQuestions = [
   },
 ]
 
-const icons = [
-  PersonIcon,
-  AssessmentOutlinedIcon,
-  CameraEnhanceOutlinedIcon,
-  BackupIcon,
-  PostAddOutlinedIcon,
-  BlurOnOutlinedIcon,
-]
-const smCardDetails = [
-  {
-    title: 'Network',
-    detail_0: 'Lorem ipsum ',
-    detail_1: 'Lorem ipsum ',
-    detail_2: 'Lorem ipsum ',
-  },
-  {
-    title: 'Data',
-    detail_0: 'Lorem ipsum ',
-    detail_1: 'Lorem ipsum ',
-    detail_2: 'Lorem ipsum ',
-  },
-  {
-    title: 'Media',
-    detail_0: 'Lorem ipsum ',
-    detail_1: 'Lorem ipsum ',
-    detail_2: 'Lorem ipsum ',
-  },
-  {
-    title: 'Cloud',
-    detail_0: 'Lorem ipsum ',
-    detail_1: 'Lorem ipsum ',
-    detail_2: 'Lorem ipsum ',
-  },
-  {
-    title: 'New Content',
-    detail_0: 'Lorem ipsum ',
-    detail_1: 'Lorem ipsum ',
-    detail_2: 'Lorem ipsum ',
-  },
-  {
-    title: 'Computer',
-    detail_0: 'Lorem ipsum ',
-    detail_1: 'Lorem ipsum ',
-    detail_2: 'Lorem ipsum ',
-  },
-]
 function Questions(props) {
   const classes = useStyles(props)
-  const [inProp, setInProp] = React.useState(false)
-
-  const handleChange = () => {
-    if (window.scrollY >= 2400) {
-      setInProp(true)
-    } else {
-      setInProp(false)
-    }
-  }
-  window.addEventListener('scroll', handleChange)
 
   return (
     <div className={classes.root}>
       <Grid className={classes.titleWrapper}>
         <SectionHeader title="Ask yourself these Q's" subTitle="Bring you ideas to life!" />
       </Grid>
-      {/* First set of questions */}
       <Container maxWidth="lg">
         <Grid container direction="row" justify="center">
           {sectionOneQuestions.map((question, index) => (
-            <Paper key={index} variant="outlined" className={classes.lgBoxes}>
+            <Paper key={question.id} variant="outlined" className={classes.lgBoxes}>
               <Typography variant="h6" className={classes.question}>
                 {question.question}
               </Typography>
@@ -137,43 +76,6 @@ function Questions(props) {
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet
             consectetur adipisicing elit.
           </Typography>
-        </Grid>
-        {/* Smaller Service cards with Icons start here */}
-        <Grid container direction="row" justify="center">
-          <div className={classes.smBoxWrapper}>
-            {smCardDetails.map((detail, index) => {
-              const Icon = icons[index]
-              return (
-                <Transition in={setInProp} timeout={1000} key={index}>
-                  {(state) => (
-                    <Fade direction="up" in={inProp} mountOnEnter unmountOnExit timeout={2800}>
-                      <Paper className={classes.smBoxes} elevation={10} key={index}>
-                        <Typography variant="h6">
-                          <Icon className={classes.icons} />
-                        </Typography>
-                        <Typography variant="h6">{detail.title}</Typography>
-
-                        <Grid container direction="row" justify="center" alignItems="flex-start">
-                          <Typography paragraph className={classes.para}>
-                            <CheckBoxIcon className={classes.smIcon} />
-                            {detail.detail_0}
-                          </Typography>
-                          <Typography paragraph className={classes.para}>
-                            <CheckBoxIcon className={classes.smIcon} />
-                            {detail.detail_1}
-                          </Typography>
-                          <Typography paragraph className={classes.para}>
-                            <CheckBoxIcon className={classes.smIcon} />
-                            {detail.detail_2}
-                          </Typography>
-                        </Grid>
-                      </Paper>
-                    </Fade>
-                  )}
-                </Transition>
-              )
-            })}
-          </div>
         </Grid>
       </Container>
     </div>
