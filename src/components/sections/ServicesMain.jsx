e5b254fcd1e2de846a8fe1c314f42a83be7560a7
Edd-wordd <edward.plasencio@gmail.com>
2023-01-10 14:19:59 -0800

    services main cleanUP

--- src/components/sections/ServicesMain.jsx
+++ src/components/sections/ServicesMain.jsx
@@ -3,24 +3,29 @@ import { Paper, Container, Typography, Grid, Grow } from '@material-ui/core'
 import { SectionHeader, ButtonLink } from '../index'
 import { Transition } from 'react-transition-group'
 import { useStyles } from '../styles/sections/ServicesMain.styles'
+import { handleChange } from "../../utils";
 
 const coreValues = [
   {
+    id: 4000,
     coreTitle: 'Best Practices',
     coreValue:
       'We believe in high quality work, and keep our team members practicing the industry standards while developing new innovative ways.',
   },
   {
+    id: 4001,
     coreTitle: 'Industry Standards',
     coreValue:
       'We believe in high quality work, and keep our team members practicing the industry standards while developing new innovative ways.',
   },
   {
+    id: 4002,
     coreTitle: 'Quality Work',
     coreValue:
       'We believe in high quality work, and keep our team members practicing the industry standards while developing new innovative ways.',
   },
   {
+    id: 4003,
     coreTitle: 'Innovative Ideas',
     coreValue:
       'We believe in high quality work, and keep our team members practicing the industry standards while developing new innovative ways.',
@@ -31,14 +36,9 @@ function ServicesMain(props) {
   const classes = useStyles(props)
   const [inProp, setInProp] = React.useState(false)
 
-  const handleChange = () => {
-    if (window.scrollY >= 400) {
-      setInProp(true)
-    } else {
-      setInProp(false)
-    }
-  }
-  window.addEventListener('scroll', handleChange)
+  window.addEventListener('scroll', function(){
+    handleChange(400, setInProp)
+  })
 
   return (
     <>
@@ -58,14 +58,13 @@ function ServicesMain(props) {
           </Typography>
         </Grid>
       </Container>
-
       <Container maxWidth="xl">
         <Grid container direction="row" justify="space-evenly" alignItems="center">
           {coreValues.map((value, index) => (
-            <Transition in={setInProp} timeout={2000} key={index}>
+            <Transition in={setInProp} timeout={2000} key={value.id}>
               {(state) => (
                 <Grow in={inProp} timeout={2000}>
-                  <Paper elevation={2} className={classes.boxes} key={index}>
+                  <Paper elevation={2} className={classes.boxes} key={value.id}>
                     <Typography variant="subtitle1">
                       <b>{value.coreTitle}</b>
                     </Typography>
