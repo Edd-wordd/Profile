ac960c54cf4963ed3d252bd62f4f8058544aaa52
Edd-wordd <edward.plasencio@gmail.com>
2023-01-11 14:27:06 -0800

    service info code clean up

--- src/components/styles/sections/MajorSigns.styles.js
+++ src/components/styles/sections/MajorSigns.styles.js
@@ -14,18 +14,17 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: '1.5rem 1.5rem',
     margin: theme.spacing(4),
     color: theme.palette.text.secondary,
-    borderRadius: theme.spacing(1),
-    boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)',
+    borderRadius: '5px',
     [theme.breakpoints.down('md')]: {
       margin: theme.spacing(1),
     },
   },
   image: {
     margin: theme.spacing(0.5),
-    borderRadius: theme.spacing(1),
+    borderRadius: '5px',
     width: '400px',
     height: '400px',
     [theme.breakpoints.down('md')]: {
@@ -34,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paperTitle: {
     fontSize: theme.spacing(3),
-    fontWeight: 'bold',
+    fontWeight: 500,
     color: theme.palette.text.primary,
     marginBottom: theme.spacing(1),
     [theme.breakpoints.down('md')]: {
