6ce40ff089b2a94610025388cff1be5f58e76756
eddwordd <edward.plasencio@gmail.com>
2023-03-18 08:16:40 +0400

    update context writing for who we are

--- src/components/sections/WhoWeAre.jsx
+++ src/components/sections/WhoWeAre.jsx
@@ -5,24 +5,29 @@ import { Transition } from 'react-transition-group'
 import { ButtonLink, SectionHeader } from '../index'
 import { useStyles } from '../styles/sections/WhoWeAre.styles'
 
+// how to new line in a string
+// https://stackoverflow.com/questions/10805125/how-to-write-multiline-strings-in-javascript
 const coreValues = [
   {
     id: 1000,
     coreTitle: 'Best Practices',
-    coreValue:
-      'We believe in high quality work, and keep our team members practicing the industry standards while developing new innovative ways.',
+    coreValue: `We always start with a deep understanding of our clients' needs and goals. 
+    - We use cutting-edge technologies to develop solutions that are scalable, secure, and future-proof. 
+    - We are committed to delivering projects on time and within budget. 
+    - We prioritize communication and transparency throughout the entire development process. 
+    - We never compromise on quality or performance.`,
   },
   {
     id: 1001,
     coreTitle: 'Industry Standards',
     coreValue:
-      'We believe in high quality work, and keep our team members practicing the industry standards while developing new innovative ways.',
+      "We adhere to the highest industry standards and take pride in our reputation for delivering reliable and secure solutions. We understand the importance of protecting our clients' sensitive data and take all necessary measures to ensure their privacy and security.",
   },
   {
     id: 1002,
     coreTitle: 'Culture',
     coreValue:
-      'We believe in high quality work, and keep our team members practicing the industry standards while developing new innovative ways.',
+      'We foster a culture of creativity, collaboration, and innovation. Our team members are encouraged to explore new ideas, experiment with different approaches, and share their knowledge and expertise with each other. This culture of learning and growth enables us to continually improve our services and provide our clients with the best possible solutions.',
   },
 ]
 
@@ -56,15 +61,16 @@ function WhoWeAre(props) {
       <Container maxWidth="lg">
         <Grid className={classes.paragraphWrapper}>
           <Typography paragraph variant="body1" className={classes.paraInfo}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, officiis ea! Atque,
-            ratione cumque. Fugiat earum eius doloribus quo repudiandae, maiores excepturi officia
-            iusto consequuntur quis odio labore, commodi minima? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Deserunt, officiis ea! Atque, ratione cumque. Fugiat earum
-            eius doloribus quo repudiandae, maiores excepturi officia iusto consequuntur quis odio
-            labore, commodi minima? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Deserunt, officiis ea! Atque, ratione cumque. Fugiat earum eius doloribus quo
-            repudiandae, maiores excepturi officia iusto consequuntur quis odio labore, commodi
-            minima?8
+            Welcome to our world of digital innovation! We are a team of skilled technologists and
+            problem solvers, passionate about harnessing the power of technology to transform
+            businesses and lives. Our mission is to provide innovative web and software solutions
+            that meet the evolving needs of our clients, no matter how complex. We thrive on
+            challenges and believe that great ideas can come from anyone, anywhere. With our
+            creative and collaborative approach, we bring fresh perspectives to every project,
+            delivering tailored solutions that exceed expectations. Whether you're looking for
+            cutting-edge web design, mobile app development, or custom software solutions, we've got
+            you covered. Let's embark on a journey of digital transformation together, and bring
+            your vision to life!
           </Typography>
         </Grid>
       </Container>
