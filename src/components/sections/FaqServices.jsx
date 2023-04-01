68f59cbeb412f8557434c6a6a75a5677bc6c891b
eddwordd <edward.plasencio@gmail.com>
2023-04-01 20:55:54 +0400

    remove yarn

--- src/components/sections/FaqServices.jsx
+++ src/components/sections/FaqServices.jsx
@@ -3,7 +3,12 @@ import { SectionHeader } from '../index'
 import { Grid, Typography, Container } from '@mui/material'
 import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { useStyles } from '../styles/sections/FaqServices.styles'
+// import { useStyles } from '../styles/sections/FaqServices.styles'
+import {
+  InfoWrapper,
+  AccordionWrapper,
+  CustomAccordion,
+} from '../styles/sections/FaqServices.styles'
 
 const questions = [
   {
@@ -39,7 +44,7 @@ const questions = [
 ]
 
 function FaqServices(props) {
-  const classes = useStyles(props)
+  // const classes = useStyles(props)
   const [expanded, setExpanded] = React.useState('panel1')
 
   const handleChange = (panel) => (event, isExpanded) => {
@@ -49,51 +54,57 @@ function FaqServices(props) {
     <div>
       <SectionHeader title="FAQ" subTitle="Ask Away" />
       <Container maxWidth="lg">
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          className={classes.infoWrapper}
-        >
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet veniam ipsum aliquid
-            explicabo. Harum ipsa blanditiis eaque ducimus dolores libero corrupti eligendi debitis.
-            Nulla delectus tempore aliquid. Beatae, blanditiis. Necessitatibus? Lorem ipsum dolor
-            sit amet consectetur, adipisicing elit. Nemo quasi beatae, doloremque aliquam deserunt
-            veritatis perspiciatis molestias sequi libero dolorem, natus quibusdam ex atque, sit
-            sapiente odit quidem eum autem.
-          </Typography>
-        </Grid>
-        <Grid
-          container
-          direction="row"
-          justify="space-evenly"
-          alignItems="center"
-          className={classes.accordionWrapper}
-        >
-          <Grid>
-            {questions.map((question) => {
-              return (
-                <Accordion
-                  className={classes.accordion}
-                  key={question.id}
-                  expanded={expanded === `${question.rank}`}
-                  onChange={handleChange(`${question.rank}`)}
-                >
-                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography>{question.question}</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <Typography>
-                      this is in the {question.question} {question.details}
-                    </Typography>
-                  </AccordionDetails>
-                </Accordion>
-              )
-            })}
+        <InfoWrapper>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            // className={classes.infoWrapper}
+          >
+            <Typography paragraph>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet veniam ipsum aliquid
+              explicabo. Harum ipsa blanditiis eaque ducimus dolores libero corrupti eligendi
+              debitis. Nulla delectus tempore aliquid. Beatae, blanditiis. Necessitatibus? Lorem
+              ipsum dolor sit amet consectetur, adipisicing elit. Nemo quasi beatae, doloremque
+              aliquam deserunt veritatis perspiciatis molestias sequi libero dolorem, natus
+              quibusdam ex atque, sit sapiente odit quidem eum autem.
+            </Typography>
           </Grid>
-        </Grid>
+        </InfoWrapper>
+        <AccordionWrapper>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            // className={classes.accordionWrapper}
+          >
+            <Grid>
+              {questions.map((question) => {
+                return (
+                  <CustomAccordion key={question.id}>
+                    <Accordion
+                      // className={classes.accordion}
+                      key={question.id}
+                      expanded={expanded === `${question.rank}`}
+                      onChange={handleChange(`${question.rank}`)}
+                    >
+                      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography>{question.question}</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <Typography>
+                          this is in the {question.question} {question.details}
+                        </Typography>
+                      </AccordionDetails>
+                    </Accordion>
+                  </CustomAccordion>
+                )
+              })}
+            </Grid>
+          </Grid>
+        </AccordionWrapper>
       </Container>
     </div>
   )
--- src/components/styles/sections/FaqServices.styles.js
+++ src/components/styles/sections/FaqServices.styles.js
@@ -1,19 +1,60 @@
-import { makeStyles } from '@mui/styles'
+// import { makeStyles } from '@mui/system'
+//
+// const useStyles = makeStyles((theme) => ({
+//   infoWrapper: {
+//     padding: '2rem 0rem',
+//   },
+//   accordionWrapper: {
+//     padding: '2rem 0rem 8rem 0rem',
+//   },
+//   accordion: {
+//     width: theme.spacing(100),
+//     backgroundColor: theme.palette.defaultLight.main,
+//     [theme.breakpoints.down('sm')]: {
+//       width: theme.spacing(40),
+//     },
+//   },
+// }))
+//
+// export { useStyles }
 
-const useStyles = makeStyles((theme) => ({
-  infoWrapper: {
-    padding: '2rem 0rem',
-  },
-  accordionWrapper: {
-    padding: '2rem 0rem 8rem 0rem',
-  },
-  accordion: {
-    width: theme.spacing(100),
-    backgroundColor: theme.palette.defaultLight.main,
-    [theme.breakpoints.down('sm')]: {
-      width: theme.spacing(40),
-    },
+// import { makeStyles } from '@mui/system'
+//
+// const useStyles = makeStyles((theme) => ({
+//   infoWrapper: {
+//     padding: '2rem 0rem',
+//   },
+//   accordionWrapper: {
+//     padding: '2rem 0rem 8rem 0rem',
+//   },
+//   accordion: {
+//     width: theme.spacing(100),
+//     backgroundColor: theme.palette.defaultLight.main,
+//     [theme.breakpoints.down('sm')]: {
+//       width: theme.spacing(40),
+//     },
+//   },
+// }))
+//
+// export { useStyles }
+
+import { styled } from '@mui/system'
+
+const InfoWrapper = styled('div')(({ theme }) => ({
+  padding: '2rem 0rem',
+}))
+
+const AccordionWrapper = styled('div')(({ theme }) => ({
+  padding: '2rem 0rem 8rem 0rem',
+}))
+
+const CustomAccordion = styled('div')(({ theme }) => ({
+  width: theme.spacing(100),
+  marginTop: theme.spacing(1),
+  backgroundColor: theme.palette.defaultLight.main,
+  [theme.breakpoints.down('sm')]: {
+    width: theme.spacing(40),
   },
 }))
 
-export { useStyles }
+export { InfoWrapper, AccordionWrapper, CustomAccordion }
