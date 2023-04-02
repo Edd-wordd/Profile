ff9e7dfe6e1e12e5ba88c46d1df43e5c66487150
eddwordd <edward.plasencio@gmail.com>
2023-04-02 22:51:58 +0400

    fix styling issues

--- src/components/sections/Questions.jsx
+++ src/components/sections/Questions.jsx
@@ -1,51 +1,73 @@
 import React from 'react'
 import { Container, Typography, Grid, Link, Paper } from '@mui/material'
 import { SectionHeader } from '../index'
-import { useStyles } from '../styles/sections/Questions.styles'
 import questionDetails from '../../data/questionsData'
+import { QuestionsBackground, QuestionLinks, Span } from '../styles/sections/Questions.styles'
 
-function Questions(props) {
-  const classes = useStyles(props)
-
+function Questions() {
   return (
-    <div className={classes.root}>
-      <Grid className={classes.titleWrapper}>
-        <SectionHeader title="Ask yourself these Q's" subTitle="Bring you ideas to life!" />
-      </Grid>
-      <Container maxWidth="lg">
-        <Grid container direction="row" justify="center">
-          {questionDetails.map((question) => (
-            <Paper key={question.id} variant="outlined" className={classes.lgBoxes}>
-              <Typography variant="h6" className={classes.question}>
-                {question.question}
-              </Typography>
-              <Typography paragraph className={classes.para}>
-                {question.details}
-              </Typography>
-              <Grid container direction="row" justify="flex-end" alignItems="center">
-                <Link href={question.link} className={classes.links}>
-                  Learn More
-                </Link>
-              </Grid>
-            </Paper>
-          ))}
+    <>
+      <QuestionsBackground>
+        <Grid sx={{ color: '#f5f5f5', padding: '2rem 0rem' }}>
+          <SectionHeader title="Ask yourself these Q's" subTitle="Bring you ideas to life!" />
         </Grid>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          className={classes.btmQuestion}
-        >
-          <Typography variant="h6" className={classes.question}>
-            If you answered <span className={classes.word}>YES</span> to any of these questions,
-            reach out to us for a consultation on how we can help you establish and enhance your
-            digital presence. then consider our services to help you get started
-            <span className={classes.word}>!!!</span>
-          </Typography>
-        </Grid>
-      </Container>
-    </div>
+        <Container maxWidth="lg">
+          <Grid container direction="row" justifyContent="center">
+            {questionDetails.map((question) => (
+              <Paper
+                key={question.id}
+                variant="outlined"
+                sx={{
+                  maxWidth: '30rem',
+                  height: 'auto',
+                  margin: '1rem 1rem',
+                  backgroundColor: 'rgba(255, 255, 255, 0.60)',
+                  padding: '1rem 1.75rem ',
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  sx={{ color: '#f5f5f5', fontWeight: 600, textAlign: 'center' }}
+                >
+                  {question.question}
+                </Typography>
+                <Typography paragraph sx={{ color: '#f5f5f5', padding: '1rem 0' }}>
+                  {question.details}
+                </Typography>
+                <Grid container direction="row" justifyContent="flex-end" alignItems="center">
+                  <Link href={question.link} component={QuestionLinks}>
+                    Learn More
+                  </Link>
+                </Grid>
+              </Paper>
+            ))}
+          </Grid>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ padding: '3rem 0rem 0rem 0rem' }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ color: '#f5f5f5', fontWeight: 600, textAlign: 'center' }}
+            >
+              If you answered
+              <Span>
+                <span> YES </span>
+              </Span>
+              to any of these questions, reach out to us for a consultation on how we can help you
+              establish and enhance your digital presence. then consider our services to help you
+              get started
+              <Span>
+                <span> !!!</span>
+              </Span>
+            </Typography>
+          </Grid>
+        </Container>
+      </QuestionsBackground>
+    </>
   )
 }
 
--- src/components/styles/sections/Questions.styles.js
+++ src/components/styles/sections/Questions.styles.js
@@ -1,54 +1,33 @@
-import { makeStyles } from '@mui/styles'
+import { styled } from '@mui/system'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundImage: `url(https://images.unsplash.com/photo-1625014618427-fbc980b974f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80)`,
-    width: '100%',
-    height: 'auto',
-    backgroundSize: 'cover',
-    backgroundAttachment: 'fixed',
-    backgroundPosition: 'center center',
-    paddingBottom: '5rem',
-  },
-  titleWrapper: {
-    color: theme.palette.defaultLight.main,
-    padding: '2rem 0rem',
-  },
-  lgBoxes: {
-    width: theme.spacing(60),
-    height: 'auto',
-    margin: '1rem 1rem',
-    backgroundColor: 'rgba(255, 255, 255, 0.30)',
-    padding: '.5rem 1rem ',
-  },
-  btmQuestion: {
-    padding: '3rem 0rem 0rem 0rem',
-  },
-  question: {
-    color: theme.palette.defaultLight.main,
-    fontWeight: 600,
-    textAlign: 'center',
-  },
-  para: {
-    color: theme.palette.defaultLight.main,
-  },
-  word: {
-    color: theme.palette.primary.main,
-    fontSize: theme.spacing(3),
-    fontWeight: '600',
-  },
-  links: {
-    color: theme.palette.defaultLight.main,
-    textDecoration: 'underline',
-    padding: '.5rem .5rem',
-    '&:hover': {
-      background: theme.palette.primary.main,
-      opacity: '.7',
-      transform: 'scale(1.05)',
-      padding: '.5rem .5rem',
-      borderRadius: '5px',
-    },
-  },
+const QuestionsBackground = styled('div')(({ theme }) => ({
+  backgroundImage: `url(https://images.unsplash.com/photo-1625014618427-fbc980b974f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80)`,
+  width: '100%',
+  height: 'auto',
+  backgroundSize: 'cover',
+  backgroundAttachment: 'fixed',
+  backgroundPosition: 'center center',
+  paddingBottom: '5rem ',
 }))
 
-export { useStyles }
+const QuestionLinks = styled('a')(({ theme }) => ({
+  color: theme.palette.defaultLight.main,
+  textDecoration: 'underline',
+  padding: '.5rem .5rem',
+  '&:hover': {
+    background: theme.palette.primary.main,
+    opacity: '.7',
+    transform: 'scale(1.05)',
+    padding: '.5rem .5rem',
+    borderRadius: '5px',
+    color: theme.palette.defaultLight.main,
+    textDecoration: 'none',
+  },
+}))
+const Span = styled('span')(({ theme }) => ({
+  color: theme.palette.primary.main,
+  fontSize: '1.5rem',
+  fontWeight: '600',
+}))
+
+export { QuestionsBackground, QuestionLinks, Span }
