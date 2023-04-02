c01d77b5afbca02dc2e7c4dbb258f2523b29afe4
eddwordd <edward.plasencio@gmail.com>
2023-04-02 19:15:58 +0400

    getStarted refactor

--- src/components/styles/sections/GetStarted.styles.js
+++ src/components/styles/sections/GetStarted.styles.js
@@ -1,38 +1,33 @@
-import { makeStyles } from '@mui/styles'
+import { styled } from '@mui/system'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.primary_100.secondary, //light purple color
-    paddingBottom: theme.spacing(5),
-  },
-  headingWrapper: {
-    padding: theme.spacing(3),
-  },
-  icons: {
-    width: theme.spacing(6),
-    height: 'auto',
-    color: theme.palette.charcoal.secondary, // light grey color
-  },
-  line: {
-    width: theme.spacing(23),
-    padding: '0rem 5rem',
-    margin: '0rem .5rem',
-    borderBottom: '3px dotted #c0c0c0',
-  },
-  boxes: {
-    width: theme.spacing(37),
-    minHeight: theme.spacing(30),
-    border: 'none',
-    padding: theme.spacing(4),
-    margin: '0rem 1.5rem 0rem 1rem',
-    marginBottom: theme.spacing(4),
-    [theme.breakpoints.down('xs')]: {
-      width: '100%',
-    },
-  },
-  iconWrapper: {
-    marginTop: theme.spacing(6),
-    marginBottom: theme.spacing(1),
+const Card = styled('div')(({ theme }) => ({
+  width: theme.spacing(37),
+  minHeight: theme.spacing(30),
+  border: 'none',
+  padding: theme.spacing(4),
+  margin: '0rem 1.5rem 0rem 1rem',
+  marginBottom: theme.spacing(4),
+  [theme.breakpoints.down('xs')]: {
+    width: '100%',
   },
 }))
-export { useStyles }
+const DottedLine = styled('div')(({ theme }) => ({
+  width: theme.spacing(23),
+  padding: '0rem 5rem',
+  margin: '0rem .5rem',
+  borderBottom: '3px dotted #c0c0c0',
+  [theme.breakpoints.down('xl')]: {
+    display: 'none',
+  },
+}))
+
+const IconWrapper = styled('div')(({ theme }) => ({
+  width: theme.spacing(3),
+  height: 'auto',
+  color: theme.palette.charcoal.secondary,
+  [theme.breakpoints.down('xl')]: {
+    display: 'none',
+  },
+}))
+
+export { DottedLine, Card, IconWrapper }
---
+++ src/data/getStartedData.js
@@ -0,0 +1,29 @@
+const getStartedSteps = [
+  {
+    id: 1000,
+    title: '1. Call us!',
+    description:
+      'Contact us by phone or email to schedule an initial consultation, where we can discuss your business needs and goals, as well as our services and pricing options.',
+  },
+  {
+    id: 1001,
+    title: '2. Planning and Design!',
+    description:
+      "Once the planning phase is complete, our company can move on to the design phase. This involves creating a visual representation of the website using wireframes or mockups. Our company will also finalize the website's layout, color scheme, and typography during this phase.\n" +
+      '\n',
+  },
+  {
+    id: 1002,
+    title: '3. Development!',
+    description:
+      " With the website's design finalized, our company can start the development phase. This involves building the website's front-end and back-end components, such as the user interface, database, and server-side scripts. We also test the website's functionality and compatibility during this phase.",
+  },
+  {
+    id: 1003,
+    title: '4. Launch your Website!',
+    description:
+      "Once the website is fully developed and tested, our company can launch it. This involves deploying the website to a web server and configuring it for optimal performance and security. We also optimize the website's search engine visibility and monitor its traffic and user behavior to ensure that it meets your website's goals.",
+  },
+]
+
+export default getStartedSteps
