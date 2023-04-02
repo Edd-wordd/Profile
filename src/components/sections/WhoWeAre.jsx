2333aae5e6b895f2ceb04122dee7b7598765b466
eddwordd <edward.plasencio@gmail.com>
2023-04-02 21:53:21 +0400

    fix who we are component

--- src/components/sections/WhoWeAre.jsx
+++ src/components/sections/WhoWeAre.jsx
@@ -3,11 +3,9 @@ import { Container, Typography, Grid, Paper } from '@mui/material'
 import { Zoom } from '@mui/material'
 import Transition from 'react-transition-group/Transition'
 import { ButtonLink, SectionHeader } from '../index'
-import { useStyles } from '../styles/sections/WhoWeAre.styles'
 import companyValues from '../../data/whoWeAreData'
-
+import { WhoWeAreCards } from '../styles/sections/WhoWeAre.styles'
 function WhoWeAre(props) {
-  const classes = useStyles(props)
   const [inProp, setInProp] = React.useState(false)
 
   const cardsObs = (entries, observer) => {
@@ -34,8 +32,8 @@ function WhoWeAre(props) {
     <>
       <SectionHeader title="Who We Are" subTitle="Bring you ideas to life!" />
       <Container maxWidth="lg">
-        <Grid className={classes.paragraphWrapper}>
-          <Typography paragraph variant="body1" className={classes.paraInfo}>
+        <Grid sx={{ margin: '3rem 0' }}>
+          <Typography paragraph variant="body1" sx={{ lineHeight: '1.75rem' }}>
             Welcome to our world of digital innovation! We are a team of skilled technologists and
             problem solvers, passionate about harnessing the power of technology to transform
             businesses and lives. Our mission is to provide innovative web and software solutions
@@ -51,12 +49,12 @@ function WhoWeAre(props) {
       </Container>
 
       <Container maxWidth="xl" id="cards">
-        <Grid container direction="row" justify="space-evenly" alignItems="center">
+        <Grid container direction="row" justifyContent="space-evenly" alignItems="center">
           {companyValues.map((value) => (
             <Transition in={!!setInProp} timeout={1000} key={value.id}>
               {() => (
                 <Zoom in={inProp} mountOnEnter unmountOnExit timeout={2000}>
-                  <Paper variant="outlined" className={classes.boxes}>
+                  <Paper variant="outlined" component={WhoWeAreCards}>
                     <Typography variant="subtitle1">
                       <b>{value.title}</b>
                     </Typography>
