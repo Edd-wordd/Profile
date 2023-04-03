91914f70ddc3146d8ad7931fe4bb01832fe81f4e
eddwordd <edward.plasencio@gmail.com>
2023-04-03 16:21:23 +0400

    remove styles not being used

--- src/components/styles/layout/Footer.styles.js
+++ src/components/styles/layout/Footer.styles.js
@@ -5,14 +5,13 @@ const FooterBackground = styled('div')(({ theme }) => ({
   color: theme.palette.defaultLight.main,
   width: '100%',
   height: 'auto',
-  paddingTop: '3rem',
+  paddingTop: '5rem',
   overflowX: 'hidden',
 }))
 
 const SectionCards = styled('div')(({ theme }) => ({
   width: '20rem',
   minHeight: '13rem',
-  padding: '5rem 0 3rem 0',
   backgroundColor: 'transparent !important',
   color: `${theme.palette.defaultLight.main} !important`,
   fontWeight: 200,
@@ -22,18 +21,9 @@ const SectionCards = styled('div')(({ theme }) => ({
   },
 }))
 
-const FooterButton = styled('button')(({ theme }) => ({
-  color: theme.palette.defaultLight.main,
-  marginTop: '1rem',
-  '&:hover': {
-    opacity: '.8',
-  },
-}))
-
 const FooterMainLogo = styled('div')(({ theme }) => ({
   width: '16rem',
   height: 'auto',
-  // paddingTop: '-3rem',
   [theme.breakpoints.down('xs')]: {
     width: '100%',
     height: 'auto',
@@ -44,19 +34,12 @@ const FooterMainLogo = styled('div')(({ theme }) => ({
   },
 }))
 
-const FooterLink = styled('a')(({ theme }) => ({
-  textDecoration: 'none',
-  color: theme.palette.defaultLight.main,
-  fontWeight: 'lighter',
-  letterSpacing: '1px',
-}))
-
 const FooterDiamond = styled('div')(({ theme }) => ({
   width: '10px',
   height: '10px',
   border: '2px solid #343a40',
   transform: 'rotate(45deg)',
-  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  background: 'linear-gradient(45deg, #89023e 30%, #c72c61 90%)',
   margin: '.3rem 1rem',
 }))
 
@@ -68,12 +51,4 @@ const FooterLine = styled('div')(({ theme }) => ({
   height: '1px',
   background: 'linear-gradient(45deg, #89023e 30%, #c72c61 90%)',
 }))
-export {
-  FooterBackground,
-  SectionCards,
-  FooterButton,
-  FooterMainLogo,
-  FooterLink,
-  FooterDiamond,
-  FooterLine,
-}
+export { FooterBackground, SectionCards, FooterMainLogo, FooterDiamond, FooterLine }
