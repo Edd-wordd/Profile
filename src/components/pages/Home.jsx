c5d5bdb069084e4cf4daa49b16ee5fd23ef7f860
eddwordd <edward.plasencio@gmail.com>
2023-04-08 19:34:15 +0400

    main display refactor

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,15 +9,15 @@ function Home() {
   return (
     <div>
       <MainDisplay
-        mobileViewTitle="Look to the Future..."
-        mobileViewSubtitle="lets go get this"
-        mainViewTitle="The future is here, and we're here to help you get there."
-        mainViewSubtitle="-Helping you design the future of the web. "
-        href={`https://images.unsplash.com/photo-1638444571685-3f2b2f5aa00c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80`}
-        buttonOneText={'Web Development'}
-        buttonLink={'/services/web-design'}
-        buttonTwoText={'SEO Services'}
-        buttonLink2={'/services/seo'}
+        mobileViewTitle="Look to the Future"
+        mobileViewSubtitle="Discover how we can help you achieve your vision"
+        mainViewTitle="Designing the Future of the Web"
+        mainViewSubtitle="Let us help you create a better digital experience"
+        href="https://images.unsplash.com/photo-1638444571685-3f2b2f5aa00c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+        buttonOneText="Web Development Services"
+        buttonLink="/services/web-design"
+        buttonTwoText="SEO Services"
+        buttonLink2="/services/seo"
       />
       <WhoWeAre />
       <HmServices />
@@ -26,4 +26,5 @@ function Home() {
     </div>
   )
 }
+
 export default Home
