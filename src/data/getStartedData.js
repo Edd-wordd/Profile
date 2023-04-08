ec50af562e9f75571e523c54d790d84e50fb3a39
eddwordd <edward.plasencio@gmail.com>
2023-04-08 19:21:46 +0400

    get started

--- src/data/getStartedData.js
+++ src/data/getStartedData.js
@@ -1,28 +1,28 @@
 const getStartedSteps = [
   {
     id: 1000,
-    title: '1. Call us!',
+    title: 'Step 1: Contact Us!',
     description:
-      'Contact us by phone or email to schedule an initial consultation, where we can discuss your business needs and goals, as well as our services and pricing options.',
+      'Get in touch with us via phone or email to schedule an initial consultation. During this call, we will discuss your business needs and goals, and provide you with information on our services and pricing options.',
   },
   {
     id: 1001,
-    title: '2. Planning and Design!',
+    title: 'Step 2: Planning and Design!',
     description:
-      "Once the planning phase is complete, our company can move on to the design phase. This involves creating a visual representation of the website using wireframes or mockups. Our company will also finalize the website's layout, color scheme, and typography during this phase.\n" +
+      "Our team will work with you to plan and design your website. We'll create a visual representation of your website using wireframes or mockups, and finalize the website's layout, color scheme, and typography. This will ensure that your website looks and feels exactly how you want it to.\n" +
       '\n',
   },
   {
     id: 1002,
-    title: '3. Development!',
+    title: 'Step 3: Development!',
     description:
-      " With the website's design finalized, our company can start the development phase. This involves building the website's front-end and back-end components, such as the user interface, database, and server-side scripts. We also test the website's functionality and compatibility during this phase.",
+      "Once the planning and design phase is complete, our team will move on to the development phase. We'll build the website's front-end and back-end components, such as the user interface, database, and server-side scripts. During this phase, we'll also test the website's functionality and compatibility to ensure that it meets your needs.",
   },
   {
     id: 1003,
-    title: '4. Launch your Website!',
+    title: 'Step 4: Launch Your Website!',
     description:
-      "Once the website is fully developed and tested, our company can launch it. This involves deploying the website to a web server and configuring it for optimal performance and security. We also optimize the website's search engine visibility and monitor its traffic and user behavior to ensure that it meets your website's goals.",
+      "Once your website is fully developed and tested, our team will launch it. We'll deploy the website to a web server and configure it for optimal performance and security. We'll also optimize your website's search engine visibility and monitor its traffic and user behavior to ensure that it meets your website's goals.",
   },
 ]
 
