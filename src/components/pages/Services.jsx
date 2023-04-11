a24b0feab4a3d2a2d218459f5c32169529e87618
eddwordd <edward.plasencio@gmail.com>
2023-04-11 22:27:12 +0400

    remove tech used component

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ServicesOffered from '../../components/sections/ServicesOffered'
 import ServicesMain from '../../components/sections/ServicesMain'
-import TechUsed from '../../components/sections/TechUsed'
 import GetStarted from '../../components/sections/GetStarted'
 import MainDisplay from '../../components/sections/MainDisplay'
 
@@ -9,15 +8,14 @@ function Services() {
   return (
     <>
       <MainDisplay
-        mobileViewTitle="What you need to grow online!"
-        mobileViewSubtitle="lets go get this"
-        mainViewTitle="what you need to grow online!"
-        mainViewSubtitle="lets go get"
-        href={`https://images.unsplash.com/photo-1601132359864-c974e79890ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80`}
+        mobileViewTitle="Empower Your Online Presence"
+        mobileViewSubtitle="Achieve digital success"
+        mainViewTitle="Empower Your Online Presence"
+        mainViewSubtitle="Achieve digital success"
+        href="https://images.unsplash.com/photo-1601132359864-c974e79890ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
       />
       <ServicesMain />
       <ServicesOffered />
-      <TechUsed />
       <GetStarted />
     </>
   )
