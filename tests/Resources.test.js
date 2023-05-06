230f3be69a16ea53f5d9bb0011fa0a580b9e76ae
eddwordd <edward.plasencio@gmail.com>
2023-05-06 21:00:34 +0700

    jest test

--- tests/Resources.test.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import testComponent from './testComponent'
-import Resources from '../src/components/sections/Resources'
-
-testComponent(<Resources />, 'Resources')
--- tests/SectionHeader.test.js
+++ tests/SectionHeader.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
 import testComponent from './testComponent'
-import Resources from '../src/components/sections/Resources'
-
-testComponent(<Resources />, 'Resources')
+import SectionHeader from '../src/components/sections/SectionHeader'
+testComponent(<SectionHeader />, 'SectionHeader')
