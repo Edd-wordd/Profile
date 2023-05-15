90214f929e3f20267dbc2dd41acdce3a2fb6f494
eddwordd <edward.plasencio@gmail.com>
2023-05-15 21:08:27 +0700

    webpack config

---
+++ public/main.0a64d913b31d259485ae.hot-update.js
@@ -0,0 +1,1 @@
+"use strict";globalThis.webpackHotUpdateprofile("main",{},(function(e){e.h=()=>"e7cfeb62c136e8b2e5b6"}));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,108 @@
+// const path = require('path')
+// const autoprefixer = require('autoprefixer')
+// const webpack = require('webpack')
+// const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+// // const dotenv = require('dotenv')
+// const Dotenv = require('dotenv-webpack')
+// const TerserPlugin = require('terser-webpack-plugin') // Add this import
+//
+// module.exports = {
+//   mode: process.env.NODE_ENV || 'development',
+//
+//   entry: {
+//     main: path.resolve(__dirname, 'src', 'main.jsx'),
+//   },
+//   output: {
+//     path: path.join(__dirname, 'public'),
+//     filename: '[name].min.js',
+//     publicPath: '/public',
+//   },
+//   optimization: {
+//     minimize: true,
+//     minimizer: [new TerserPlugin()], // Add this line
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             plugins: ['react-refresh/babel'],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+//         loader: 'file-loader',
+//         options: {
+//           name: 'media/[name].[hash:8].[ext]',
+//           esModule: false,
+//         },
+//       },
+//
+//       {
+//         test: /\.css$/,
+//         exclude: /node_modules/,
+//         use: [
+//           'style-loader',
+//           'css-loader',
+//           {
+//             loader: 'postcss-loader',
+//             options: {
+//               plugins: () => [autoprefixer({})],
+//             },
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.(png|jpe?g|gif)$/i,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['*', '.js', '.jsx'],
+//   },
+//   devServer: {
+//     contentBase: path.join(__dirname, 'public'),
+//     historyApiFallback: true,
+//     hot: true,
+//     publicPath: '/public',
+//     port: 3000,
+//   },
+//
+//   plugins: [
+//     new Dotenv(),
+//     new webpack.HotModuleReplacementPlugin({
+//       multiStep: true,
+//       fullBuildTimeout: 3000,
+//       requestTimeout: 1000,
+//     }),
+//     new ReactRefreshWebpackPlugin(),
+//     process.env.NODE_ENV === 'development' && new ReactRefreshWebpackPlugin(),
+//   ].filter(Boolean),
+//
+//   performance: {
+//     maxAssetSize: 1000000,
+//   },
+// }
 const path = require('path')
 const autoprefixer = require('autoprefixer')
 const webpack = require('webpack')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-// const dotenv = require('dotenv')
+const dotenv = require('dotenv')
 const Dotenv = require('dotenv-webpack')
-const TerserPlugin = require('terser-webpack-plugin') // Add this import
+const TerserPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -18,8 +116,8 @@ module.exports = {
     publicPath: '/public',
   },
   optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()], // Add this line
+    minimize: !isDevelopment,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
   module: {
     rules: [
@@ -29,33 +127,14 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            plugins: ['react-refresh/babel'],
+            plugins: [
+              isDevelopment && 'react-refresh/babel',
+              !isDevelopment && ['transform-react-remove-prop-types', { mode: 'unsafe-wrap' }],
+            ].filter(Boolean),
           },
         },
       },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'file-loader',
-        options: {
-          name: 'media/[name].[hash:8].[ext]',
-          esModule: false,
-        },
-      },
-
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [autoprefixer({})],
-            },
-          },
-        ],
-      },
+      // ... other rules
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
@@ -64,8 +143,26 @@ module.exports = {
           },
         ],
       },
+
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer()],
+              },
+            },
+          },
+        ],
+      },
     ],
   },
+  // ... other configurations
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
@@ -76,7 +173,6 @@ module.exports = {
     publicPath: '/public',
     port: 3000,
   },
-
   plugins: [
     new Dotenv(),
     new webpack.HotModuleReplacementPlugin({
@@ -84,11 +180,10 @@ module.exports = {
       fullBuildTimeout: 3000,
       requestTimeout: 1000,
     }),
-    new ReactRefreshWebpackPlugin(),
-    process.env.NODE_ENV === 'development' && new ReactRefreshWebpackPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    !isDevelopment &&
+      new MiniCssExtractPlugin({
+        filename: '[name].min.css',
+      }),
   ].filter(Boolean),
-
-  performance: {
-    maxAssetSize: 1000000,
-  },
 }
