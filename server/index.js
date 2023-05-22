4e3096e7ec36aebae91fa3cbde7ed9915249ca6e
eddwordd <edward.plasencio@gmail.com>
2023-05-22 16:19:28 -0400

    dotenv

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const webpackConfig = require('../webpack.config')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const fs = require('fs')
+require('dotenv').config()
 
 dotenv.config()
 
