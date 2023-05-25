902becd831ffd846336247c41a34cf0d90d339b8
eddwordd <edward.plasencio@gmail.com>
2023-05-25 14:50:34 -0400

    new api

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -169,7 +169,7 @@ function ContactForm(props) {
       setIsLoading(false)
       try {
         axios
-          .post(' https://qht3kda9vh.execute-api.us-west-1.amazonaws.com/monatechapi', simpleData)
+          .post('/api/form', simpleData)
           .then((res) => {
             // console.log(res, 'data is here')
             setAlert(true)
