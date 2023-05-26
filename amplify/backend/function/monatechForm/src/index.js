b8f6063d0fdff85afab370d747b5815ab0a4c809
eddwordd <edward.plasencio@gmail.com>
2023-05-26 19:35:09 -0400

    routes

--- amplify/backend/function/monatechForm/src/index.js
+++ amplify/backend/function/monatechForm/src/index.js
@@ -1,9 +1,9 @@
 import awsServerlessExpress from 'aws-serverless-express'
-import app from './app.js'
+import app from '../app.js' // Adjust the file path based on your directory structure
 
 const server = awsServerlessExpress.createServer(app)
 
-export const handler = async (event, context) => {
+export const handler = (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`)
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise
+  awsServerlessExpress.proxy(server, event, context)
 }
